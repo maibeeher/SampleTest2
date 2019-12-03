64b3e193061cfc6bbc8015d48bb9ffe203fc5fd6
unknown <maibeeher@mail.fresnostate.edu>
2019-12-03 03:23:47 -0800

    completed exam

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastService } from '../toast/toast.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-nav',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainNavComponent implements OnInit {
 
-  constructor() { }
+  constructor(private toastService: ToastService, private router: Router) { }
 
   ngOnInit() {
   }
 
+  showAbout() {
+    this.toastService.showToast('success', 5000, 'This application was created by MaiBee Her (c).');
+  }
+logout() {
+  localStorage.clear();
+  localStorage.setItem('user', JSON.stringify({}));
+  this.router.navigate(['login']);
+}
 }
